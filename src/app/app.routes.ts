9e44e7db0509b14d0ff982a4819486ac58aa33a0
{ dev : F.L.C } <20364582+dev-flc@users.noreply.github.com>
2024-03-22 14:15:18 -0600

    feat(others): 🎸 update config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './view/home/home.component';
+import { NotfoundComponent } from './view/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+];
