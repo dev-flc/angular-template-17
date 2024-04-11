e27855fa9538a331dd659053cab3dd2c12f65b7a
{ dev : F.L.C } <20364582+dev-flc@users.noreply.github.com>
2024-04-10 23:54:12 -0600

    feat(view): 🎸 home

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -6,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -15,6 +16,7 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrl: './navigation.component.scss',
   standalone: true,
   imports: [
+    RouterOutlet,
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
@@ -29,6 +31,7 @@ export class NavigationComponent {
   }
   private breakpointObserver = inject(BreakpointObserver);
 
+  // #region breakpoint
   currentBreakpoint$: Observable<string> = this.breakpointObserver
     .observe([
       Breakpoints.Large,
